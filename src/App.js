3734f90844cc8d9539c9893d15fcf9703f448ed5
bipin <bpnsilwal12@gmail.com>
2023-02-03 17:15:55 +0545

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './components/layouts/About';
+import Footer from './components/layouts/Footer';
+import Home from './components/layouts/Home';
+import Navbar from './components/layouts/Navbar';
+import Notfound from './components/layouts/Notfound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div>
+          <div className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/notfound" element={<Notfound />} />
+                <Route path="/*" element={<Notfound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/layouts/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <div className="text-6xl mb-4">Github Finder</div>
+    </>
+  );
+};
+
+export default About;
